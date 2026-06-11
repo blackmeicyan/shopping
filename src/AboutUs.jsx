9e70b7ffea91a08b28624052a9cd6f82577cbab2
blackmeicyan <blackmeicyan@gmail.com>
2026-06-11 17:11:43 +1000

    update files

---
+++ src/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {/* <h1 className="about-us-heading">About Us</h1> */}
+      <p className="about-us-description">Welcome to Natural House, where nature is home!</p>
+      <p className="about-us-content">
+        At Natural House we are committed to bringing natural beauty back into the lives of our customers. We pride ourselves
+        in providing products that suit the needs of our customers.
+      </p>
+      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      <p className="about-us-content">
+        Our products are wonderful additions to your home or office.
+      </p>
+      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+
+      <p className="about-us-content">
+        Join us in bringing nature back into our lives, our homes, and our offices.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
